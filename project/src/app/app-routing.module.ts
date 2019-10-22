39b0f8f27a3fec0cea99dd9b5e78ff794ac67015
Antonius Filian <antoniusfilian@gmail.com>
2019-10-22 20:54:06 +0700

    auth done

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,9 +1,22 @@
+import { AuthGuard } from './auth/auth.guard';
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
+  {
+    path: 'home',
+    canLoad: [AuthGuard],
+    loadChildren: './home/home.module#HomePageModule',
+  },
+  {
+    path: 'set-up-profile',
+    canLoad: [AuthGuard],
+    loadChildren: './set-up-profile/set-up-profile.module#SetUpProfilePageModule' 
+  },
+
+
 ];
 
 @NgModule({
